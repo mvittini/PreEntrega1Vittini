71e073f4866722d01fdfe11af5526d424e63a025
mvittini <mvittini@live.cl>
2024-05-16 16:57:09 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+        
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
